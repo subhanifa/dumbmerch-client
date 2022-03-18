699c516fcc8b75e10bb5b7544808ac17428ee39c
unknown <subhanhanifa@gmail.com>
2022-03-18 09:39:12 +0700

    Updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 export const API = () => {
   const baseUrl =
     process.env.SERVER_URL ||
-    'https://demo-backend-deploy.herokuapp.com/api/v1' ||
+    'https://dumbmerch-hanif-b30.herokuapp.com/api/v1' ||
     'https://localhost:5000/api/v1';
 
   const executeAPI = async (endpoint, config) => {
